266897cbc6af719a903f5640f34d76938906d585
José Luis Sampedro <jsampedro@protonmail.com>
2023-10-30 20:43:55 +0000

    AD

--- pw_AD_Get-Aduser.ps1
+++ pw_AD_Get-Aduser.ps1
@@ -1,1 +1,24 @@
-Get-ADUser -Identity 43765049 -Properties CannotChangePassword, Enabled, LastBadPasswordAttempt, LastLogonDate, LogonCount, MemberOf, Modified, PasswordExpired, PasswordLastSet, PasswordNeverExpires, PasswordLastSet, whenChanged, whenCreated 
+# Explaints the use of Active Directory Power Shell.
+
+# Use Get-Command with the Module parameter to determine what commands were added as part of
+#the ActiveDirectory PowerShell module when the remote server administration tools were installed.
+
+# Change identity by your AD id.
+
+Get-Command -Module ActiveDirectory
+
+    Read-Host -Prompt "Press any key to continue..."
+
+get-aduser -identity 43765049 -Properties *
+
+    Read-Host -Prompt "Press any key to continue..."
+
+Get-ADUser -Identity 43765049 -Properties CannotChangePassword, Enabled, LastBadPasswordAttempt, LastLogonDate, LogonCount, MemberOf, Modified, PasswordExpired, PasswordLastSet, PasswordNeverExpires, PasswordLastSet, whenChanged, whenCreated
+
+    Read-Host -Prompt "Press any key to continue..."
+
+Get-ADUser -Identity 43765049 | Get-Member
+
+    Read-Host -Prompt "Press any key to continue..."
+
+    Get-ADUser -Identity mike -Properties * | Get-Member
