b68890e61279e2329501d8a3a96a8f3b720aff5d
José Luis Sampedro <jsampedro@protonmail.com>
2023-11-01 22:03:58 +0000

    Advanced Funtions

---
+++ pw_Funtions-Advanced.ps1
@@ -0,0 +1,18 @@
+<# Advanced Functions
+Turning a function in PowerShell into an advanced function is really simple. One of the differences
+in a function and an advanced function is that advanced functions have a number of common
+parameters that are added to the function automatically. These common parameters include
+parameters such as Verbose and Debug. #>
+
+# Add CmdletBinding to turn the function into an advanced function.
+function Test-MECmdletBinding {
+        [CmdletBinding()] #<<-- This turns a regular function into an advanced function
+        param (
+        $ComputerName
+    )
+    Write-Output $ComputerName
+}
+
+Get-Command -Name Test-MECmdletBinding -Syntax
+
+(Get-Command -Name Test-MECmdletBinding).Parameters.Keys
