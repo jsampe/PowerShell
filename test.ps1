536485e79c319264442d7e9c3fb455919ec47d4d
José Luis Sampedro <jsampedro@protonmail.com>
2023-11-01 22:11:08 +0000

    Funtiosns Mandatory

--- test.ps1
+++ test.ps1
@@ -1,16 +1,15 @@
-<# SupportsShouldProcess
-SupportsShouldProcess adds WhatIf and Confirm parameters. This is only needed for commands
-that make changes.#>
-function Test-MESupportsShouldProcess {
-        [CmdletBinding(SupportsShouldProcess)]
+<# it’s perfectly valid to not specify a computer name at all and at least
+one computer name needs to be specified otherwise the function can’t possibly complete successfully.
+This is where the Mandatory parameter attribute comes in handy
+ #>
+
+function Test-MEParameterValidation {
+    [CmdletBinding()]
         param (
-        $ComputerName
+        [Parameter(Mandatory)]
+        [string]$ComputerName
         )
     Write-Output $ComputerName
 }
 
-# Notice that there are now WhatIf and Confirm parameters
-
-Get-Command -Name Test-MESupportsShouldProcess -Syntax
-
-(Get-Command -Name Test-MESupportsShouldProcess).Parameters.Keys
+Test-MEParameterValidation
