8cf339d856af7b090787430ea640e2b2b274b591
jsampe <jsampedro@protonmail.com>
2023-11-02 21:31:39 +0000

    Test file

--- test.ps1
+++ test.ps1
@@ -1,16 +1,1 @@
-<# Defining Public and Private Functions
-You may have helper functions that you may want to get private that are only accessible by other
-functions within the module and not accessible to be called directly by the users of your module.
-There are a couple of different ways to accomplish this.
-If you’re not following the best practices and only have a PSM1 file then your only option is to use
-the Export-ModuleMember cmdlet.#>
-
-
-function Get-MrPSVersion {
-    $PSVersionTable
-}
-function Get-MrComputerName {
-    $env:COMPUTERNAME
-}
-
-Export-ModuleMember -Function Get-MrPSVersion
+<# Only for test #>
